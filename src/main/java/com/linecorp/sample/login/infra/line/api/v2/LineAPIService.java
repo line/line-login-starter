de7bd48fe3c2b00b9ddbf0783553e38220e54455
Ben Niu <Ben_niu@dectech.tokyo>
2018-05-16 16:03:02 +0900

    Change bot promt to aggressive

--- src/main/java/com/linecorp/sample/login/infra/line/api/v2/LineAPIService.java
+++ src/main/java/com/linecorp/sample/login/infra/line/api/v2/LineAPIService.java
@@ -113,7 +113,7 @@ public class LineAPIService {
                 + "&redirect_uri=" + encodedCallbackUrl
                 + "&state=" + state
                 + "&scope=" + scope
-                + "&bot_prompt=" + "normal"
+                + "&bot_prompt=" + "aggressive"
                 + "&nonce=" + nonce;
     }
 
